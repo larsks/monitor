cfa83a7edf60239c7e1a086781007271ff446a6e
Lars Kellogg-Stedman <lars@oddbit.com>
2011-09-26 22:22:54 -0400

    basic functionality is in place

--- monitor.c
+++ monitor.c
@@ -11,15 +11,21 @@
 #include <getopt.h>
 #include <string.h>
 
+#include <sys/wait.h>
+
 #define	OPT_HELP	'h'
 #define OPT_PIDFILE	'p'
 #define OPT_FOREGROUND	'f'
+#define OPT_RESTART	'R'
+#define OPT_MAXINTERVAL	'I'
 
-#define OPTSTRING	"hp:f"
+#define OPTSTRING	"hp:fRI:"
 
 char	*pidfile	= NULL;
 FILE	*pidfd		= NULL;
 int	foreground	= 0;
+int	alwaysrestart	= 0;
+int	maxinterval	= 60;
 
 void usage (FILE *out) {
 	fprintf(out, "monitor: usage: monitor [-p pidfile] [-d dir] [-f] command [options]\n");
@@ -28,6 +34,56 @@ void usage (FILE *out) {
 void die(char *msg) {
 	fprintf(stderr, "monitor: ERROR: %s\n", msg);
 	exit(1);
+}
+
+void loop (int argc, char **argv) {
+	pid_t	pid, pid2;
+	int	status;
+	int	interval = 2;
+	time_t	lastrestart = 0;
+
+	fprintf(stderr, "entering loop\n");
+	while (1) {
+		pid = fork();
+
+		switch (pid) {
+			case -1:
+				exit(1);
+			case 0:
+				execvp(argv[0], argv);
+				break;
+			default:
+				pid2 = waitpid(pid, &status, 0);
+				fprintf(stderr, "got status = %d\n", status);
+				if (status == 0)
+					goto loop_exit;
+
+				if (WIFSIGNALED(status)) {
+					fprintf(stderr, "process exited due to signal = %d\n",
+							WTERMSIG(status));
+				} else if (WIFEXITED(status)) {
+					fprintf(stderr, "process exited with status = %d\n",
+							WEXITSTATUS(status));
+				}
+
+				fprintf(stderr, "%d %d %d\n", time(NULL), (int)lastrestart, interval);
+				if (time(NULL) - lastrestart <= interval) {
+					fprintf(stderr,"sleeping %d seconds before restart.\n", interval);
+					interval = interval
+						? ( interval >= maxinterval
+							? maxinterval
+							: 2 * interval )
+						: 1;
+					sleep(interval);
+				}
+
+				lastrestart = time(NULL);
+				break;
+		}
+	}
+
+loop_exit:
+	return;
 }
 
 int main (int argc, char **argv) {
@@ -44,7 +100,12 @@ int main (int argc, char **argv) {
 			case OPT_FOREGROUND:
 				foreground = 1;
 				break;
-
+			case OPT_RESTART:
+				alwaysrestart = 1;
+				break;
+			case OPT_MAXINTERVAL:
+				maxinterval = atoi(optarg);
+				break;
 			case '?':
 				usage(stderr);
 				exit(2);
@@ -63,6 +124,7 @@ int main (int argc, char **argv) {
 		fclose(pidfd);
 	}
 
+	loop(argc-optind, argv+optind);
 	return 0;
 }
 
