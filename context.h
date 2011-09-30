37c9002f615c2a5633fd8605e139dfd078f4a00d
Lars Kellogg-Stedman <lars@oddbit.com>
2011-09-30 17:14:44 -0400

    some comments and state changes

--- context.h
+++ context.h
@@ -3,27 +3,30 @@
 
 #include <czmq.h>
 
-#define STATE_STARTING	0
-#define STATE_STARTED	1
-#define STATE_STOPPING	2
-#define STATE_STOPPED	3
-#define STATE_SLEEPING	4
-#define STATE_QUITTING	5
+#define STATE_INITIAL	0
+#define STATE_STARTING	1
+#define STATE_STARTED	2
+#define STATE_STOPPING	3
+#define STATE_STOPPED	4
+#define STATE_SLEEPING	5
+#define STATE_QUITTING	6
 
 #define CONTEXT(x) ((struct child_context *)x)
 
 struct child_context {
-	int	state;	// where we're at
-	int	target;	// where we're going
+	int	state;		// where we're at
+	int	state_prev;	// where we've been
+	int	state_next;	// where we're going
 
 	int	argc;
 	char	**argv;
-	pid_t	pid;
-	int	status;
 
-	time_t	lastexit;
-	time_t	interval;
-	time_t	ticks;
+	int	running;	// TRUE if the process is running.
+	pid_t	pid;		// PID of child process.
+	int	status;		// Exit status (from waitpid()) of child process.
+
+	time_t	lastexit;	// time of last process exit.
+	time_t	interval;	// how long to wait until next start.
 
 	zloop_t	*loop;
 };
